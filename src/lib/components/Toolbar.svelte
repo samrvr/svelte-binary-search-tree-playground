31bdc1d209ee0111fa7a9bdb9bf4d660fdf484f5
Sam R <samrevier@gmail.com>
2024-03-14 13:40:57 -0500

    feature: added center root button

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -12,14 +12,19 @@
 			insertValue = undefined;
 		}
 	}
+
+	function rootDispatch(e: any) {
+		e.preventDefault();
+		dispatch('goToRoot');
+	}
 </script>
 
-<div class="absolute bottom-6 left-6 toolbar z-10">
+<div class="absolute bottom-6 left-6 toolbar z-10 flex items-end">
 	<form on:submit={insertDispatch}>
 		<label class="text-slate-400 mb-1.5 text-sm inline-block" for="insert">Insert tree value</label>
 		<div>
 			<input
-				class="bg-slate-800 border-slate-700 text-slate-200 rounded-md py-1.5 px-1.5 focus:outline-slate-500 focus:ring-indigo-500 focus:outline-0 focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-900 w-24 transition-all duration-200 ease-in-out"
+				class="bg-slate-800 border-slate-700 text-slate-200 rounded-md py-1.5 px-1.5 focus:outline-slate-500 focus:ring-indigo-600 focus:outline-0 focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-900 w-24 transition-all duration-200 ease-in-out"
 				name="insert"
 				id="insert"
 				bind:value={insertValue}
@@ -28,6 +33,7 @@
 				max="1000"
 			/>
 			<Button type="submit">Insert</Button>
+			<Button color="secondary" on:click={rootDispatch}>Center Root</Button>
 		</div>
 	</form>
 </div>
