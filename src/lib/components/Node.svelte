74b5229233602198aef51ae63313b36b81e9ee01
Sam R <samrevier@gmail.com>
2024-03-14 13:51:09 -0500

    fix: node rendering

--- src/lib/components/Node.svelte
+++ src/lib/components/Node.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { PlaygroundConfig } from '$lib/enums/playground.config';
 	import { tree } from '$lib/store/bstStore.svelte';
-	import { canvasStore } from '$lib/store/canvasStore.svelte';
 	import { Layer } from 'svelte-canvas';
 
 	interface Props {
@@ -14,24 +13,22 @@
 
 	let hovered = $state(false);
 
-	let render =
-		() =>
-		({ context }: { context: CanvasRenderingContext2D }) => {
-			// context.fillStyle = hovered
-			// 	? PlaygroundConfig.HOVER_NODE_BG_COLOR
-			// 	: PlaygroundConfig.NODE_BG_COLOR;
-			context.fillStyle = PlaygroundConfig.NODE_BG_COLOR;
-			context.beginPath();
-			context.arc(x, y, PlaygroundConfig.NODE_SIZE, 0, 2 * Math.PI);
-			context.closePath();
-			context.fill();
+	let render = ({ context }: { context: CanvasRenderingContext2D }) => {
+		// context.fillStyle = hovered
+		// 	? PlaygroundConfig.HOVER_NODE_BG_COLOR
+		// 	: PlaygroundConfig.NODE_BG_COLOR;
+		context.fillStyle = PlaygroundConfig.NODE_BG_COLOR;
+		context.beginPath();
+		context.arc(x, y, PlaygroundConfig.NODE_SIZE, 0, 2 * Math.PI);
+		context.closePath();
+		context.fill();
 
-			context.font = `25px Jetbrains Mono, monospace`;
-			context.textAlign = 'center';
-			context.textBaseline = 'middle';
-			context.fillStyle = PlaygroundConfig.NODE_TEXT_COLOR;
-			context.fillText(value!.toString(), x, y);
-		};
+		context.font = `25px Jetbrains Mono, monospace`;
+		context.textAlign = 'center';
+		context.textBaseline = 'middle';
+		context.fillStyle = PlaygroundConfig.NODE_TEXT_COLOR;
+		context.fillText(value!.toString(), x, y);
+	};
 
 	function onEnter() {
 		hovered = true;
