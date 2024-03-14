744a5797aa3e6d38d5cbe80fa12dace98db56d88
Sam R <samrevier@gmail.com>
2024-03-14 13:42:29 -0500

    fix: center root on init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,11 @@
 	import NodeParent from '$lib/components/NodeParent.svelte';
 	import { tree } from '$lib/store/bstStore.svelte';
 	import { Canvas } from 'svelte-canvas';
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import Toolbar from '$lib/components/Toolbar.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import { canvasStore } from '$lib/store/canvasStore.svelte';
+	import RootMarker from '$lib/components/RootMarker.svelte';
 
 	let canvas: Canvas;
 	let canvasContainer: HTMLDivElement;
@@ -19,7 +20,7 @@
 			canvas: true,
 			minScale: 0.5,
 			maxScale: 1.5,
-			startX: 2500,
+			startX: -2500 + window.innerWidth / 2,
 			excludeClass: 'toolbar'
 		});
 		canvasContainer.addEventListener('wheel', panzoom.zoomWithWheel);
@@ -34,7 +35,10 @@
 		tree.insert(e.detail.value);
 	}
 
-	tree.insert(10);
+	function goToRoot() {
+		panzoom.zoom(1);
+		panzoom.pan(-2500 + window.innerWidth / 2, 0);
+	}
 </script>
 
 <svelte:head>
@@ -45,11 +49,12 @@
 <div class="h-screen w-screen overflow-hidden relative bg-slate-900" bind:this={canvasContainer}>
 	<Canvas pixelRatio={1.5} bind:this={canvas} width={5000} height={3000} layerEvents>
 		<Grid />
+		<RootMarker />
 		{#if tree.root}
 			<NodeParent x={100} y={100} depth={0} node={tree.root} side={null} />
 		{/if}
 	</Canvas>
 
 	<Header />
-	<Toolbar on:insert={insert} />
+	<Toolbar on:insert={insert} on:goToRoot={goToRoot} />
 </div>
