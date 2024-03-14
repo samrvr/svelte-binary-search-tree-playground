c5cf9cb689d86a31bbb6042e4894ee63039d7ed0
Sam R <samrevier@gmail.com>
2024-03-14 12:19:42 -0500

    accessibility improvements

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -20,13 +20,14 @@
 			<input
 				class="bg-slate-800 border-slate-700 text-slate-200 rounded-md py-1.5 px-1.5 focus:outline-slate-500 focus:ring-indigo-500 focus:outline-0 focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-900 w-24 transition-all duration-200 ease-in-out"
 				name="insert"
+				id="insert"
 				bind:value={insertValue}
 				type="number"
 				step=".01"
 				max="1000"
 			/>
 			<button
-				class="bg-indigo-500 text-indigo-200 px-3.5 py-1.5 rounded-md font-semibold cursor-pointer hover:bg-indigo-600 hover:text-indigo-300 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-slate-900 transition-all duration-200 ease-in-out"
+				class="bg-indigo-600 text-indigo-100 px-3.5 py-1.5 rounded-md font-semibold cursor-pointer hover:bg-indigo-700 hover:text-indigo-200 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-slate-900 transition-all duration-200 ease-in-out"
 				type="submit">Insert</button
 			>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import NodeParent from '$lib/components/NodeParent.svelte';
 	import { tree } from '$lib/store/bstStore.svelte';
 	import { Canvas } from 'svelte-canvas';
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import Toolbar from '$lib/components/Toolbar.svelte';
 	import Header from '$lib/components/Header.svelte';
 
@@ -17,24 +17,32 @@
 			contain: 'outside',
 			canvas: true,
 			minScale: 0.5,
-			maxScale: 2,
-			startX: 1,
-			startY: 100,
+			maxScale: 1.5,
+			startX: 2500,
 			excludeClass: 'toolbar'
 		});
 		canvasContainer.addEventListener('wheel', panzoom.zoomWithWheel);
 		canvas.redraw();
-	});
 
-	tree.insert(10);
+		return () => {
+			canvasContainer.removeEventListener('wheel', panzoom.zoomWithWheel);
+		};
+	});
 
 	function insert(e: CustomEvent<{ value: number }>) {
 		tree.insert(e.detail.value);
 	}
+
+	tree.insert(10);
 </script>
 
+<svelte:head>
+	<title>Binary Search Tree Playground</title>
+	<meta name="description" content="Visualize binary search trees with ease." />
+</svelte:head>
+
 <div class="h-screen w-screen overflow-hidden relative bg-slate-900" bind:this={canvasContainer}>
-	<Canvas pixelRatio={2} bind:this={canvas} width={5000} height={3000} layerEvents>
+	<Canvas pixelRatio={1.5} bind:this={canvas} width={5000} height={3000} layerEvents>
 		<Grid />
 		{#if tree.root}
 			<NodeParent x={100} y={100} depth={0} node={tree.root} side={null} />
